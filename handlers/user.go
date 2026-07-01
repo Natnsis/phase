232d803d58035cb92d78ad34a43cb2c50f7f62fe
Natnael <nsisay49@gmail.com>
2026-07-01 22:53:35 +0300

    handlers

--- handlers/user.go
+++ handlers/user.go
@@ -1,3 +1,15 @@
 package handlers
 
+import (
+	"encoding/json"
+	"net/http"
+
+	"pases/models"
+)
+
 // request + response with controlled json file
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	var user models.User
+	json.NewDecoder(r.Body).Decode(&user)
+	// result :=
+}
