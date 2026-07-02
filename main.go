f0a73a223557e80611db5bc6e1e2814cb6ab3420
Natnael <nsisay49@gmail.com>
2026-07-02 10:37:35 +0300

    clearing up phaes one

--- main.go
+++ main.go
@@ -5,15 +5,11 @@ import (
 	"log"
 	"net/http"
 
-	"pases/routes"
-
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	router := mux.NewRouter()
-	routes.MessageRoutes(router)
-	routes.NewRoutes(router)
 	fmt.Println("server is running on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
