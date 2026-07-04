d766d64bef36188967487bb429ca64b790a00cd8
Natnael <nsisay49@gmail.com>
2026-07-04 23:17:12 +0300

    some file tweaks

--- main.go
+++ main.go
@@ -1,18 +1,7 @@
 package main
 
-import (
-	"fmt"
-
-	"pases/handlers"
-	//	"log"
-	//	"net/http"
-	//	"github.com/gorilla/mux"
-)
+import "pases/handlers"
 
 func main() {
-	//	router := mux.NewRouter()
-	b := "nati"
-	handlers.UploadFile()
-	fmt.Println("server is running on port 8080")
-	// log.Fatal(http.ListenAndServe(":8080", router))
+	handlers.UploadFunction()
 }
